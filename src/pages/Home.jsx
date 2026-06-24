ed2b58ac0636e9778bd7afc3134e710a4ae909c1
Mohamed Magdy Elsayed <mohamedmagdyelsayed7@gmail.com>
2026-06-24 12:25:28 +0300

    feat: added PlantReferenceSection (html ,css and responsive )

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,17 @@
 import HeroSection from "../components/HeroSection";
 import BenefitSection from "../components/BenefitSection";
 import BestSellerSection from "../components/BestSellerSection";
+import PlantReferenceSection from "../components/PlantReferenceSection";
+
+
+
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <BenefitSection />
       <BestSellerSection />
+      <PlantReferenceSection />
     </main>
   );
 }
