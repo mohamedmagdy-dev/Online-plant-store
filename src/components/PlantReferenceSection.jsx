344028d4ccc09f0607a6b966851cafc4ec8589a0
Mohamed Magdy Elsayed <mohamedmagdyelsayed7@gmail.com>
2026-07-07 06:28:22 +0300

    chore: Make Plant Reference boxs link

--- src/components/PlantReferenceSection.jsx
+++ src/components/PlantReferenceSection.jsx
@@ -3,6 +3,9 @@ import LivingRoomImg from "../assets/imgs/living-room.png";
 import GuestRoomImg from "../assets/imgs/guest-room.png";
 import BedRoomImg from "../assets/imgs/bed-room.png";
 
+// React router
+import { Link } from "react-router";
+
 export default function PlantReferenceSection() {
   return (
     <section className="pb-35">
@@ -16,31 +19,34 @@ export default function PlantReferenceSection() {
       </div>
       <div className="container px-5 min-h-248.75 grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div className="flex flex-col">
-          <div
+          <Link
+            to={"/"}
             className="card h-137.25 lg:h-[50%] mb-8 bg-cover bg-no-repeat bg-center rounded-[50px]"
             style={{ backgroundImage: `url(${LivingRoomImg})` }}
           >
             <div className="overlay w-full h-full flex justify-center items-center text-white font-semibold text-5xl md:text-[64px] bg-[#0000004D] rounded-[50px]">
               <h3>Living Room</h3>
             </div>
-          </div>
-          <div
+          </Link>
+          <Link
+            to={"/"}
             className="card h-137.25 lg:h-[50%] bg-cover bg-no-repeat bg-center rounded-[50px]"
             style={{ backgroundImage: `url(${BedRoomImg})` }}
           >
             <div className="overlay w-full h-full flex justify-center items-center text-white font-semibold text-5xl md:text-[64px] bg-[#0000004D] rounded-[50px]">
               <h3>Bed Room</h3>
             </div>
-          </div>
+          </Link>
         </div>
-        <div
+        <Link
+          to={"/"}
           className="card h-137.25 lg:h-full bg-cover bg-no-repeat bg-center rounded-[50px]"
           style={{ backgroundImage: `url(${GuestRoomImg})` }}
         >
           <div className="overlay w-full h-full flex justify-center items-center text-white font-semibold text-5xl md:text-[64px] bg-[#0000004D] rounded-[50px]">
             <h3>Guest Room</h3>
           </div>
-        </div>
+        </Link>
       </div>
     </section>
   );
