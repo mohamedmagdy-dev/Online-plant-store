4b7fc206f7c5f8a43fb0ffe3d64a57c062b2292b
Mohamed Magdy Elsayed <mohamedmagdyelsayed7@gmail.com>
2026-07-09 14:08:35 +0300

    feat: added not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toggleHeaderTheme } from "../features/theme/themeSlice";
+
 export default function NotFound() {
-  return <main></main>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(toggleHeaderTheme(false));
+  }, [dispatch]);
+
+  return (
+    <main className="py-35">
+      <div className="container mx-auto px-4 flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold text-abyss mb-10 ">404 - Page Not Found</h1>
+        <p className="text-abyss text-2xl font-semibold">The page you are looking for does not exist.</p>
+      </div>
+    </main>
+  );
 }
